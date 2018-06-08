c97fb322b66a29e956d3442e1bc8506145a2afa8
Hung Nguyen <nthung.vlvn@gmail.com>
2018-06-09 00:11:00 +0700

    Add model for authentication and issues

---
+++ issue.go
@@ -0,0 +1,74 @@
+package main
+
+// SimpleIssue ...
+type SimpleIssue struct {
+	ID     string      `json:"id"`
+	Key    string      `json:"key"`
+	Self   string      `json:"self"`
+	Fields SimpleField `json:"fields"`
+}
+
+// SimpleField ...
+type SimpleField struct {
+	Summary   string    `json:"summary"`
+	Status    Status    `json:"status"`
+	Priority  Priority  `json:"priority"`
+	IssueType IssueType `json:"issuetype"`
+}
+
+// Issue ...
+type Issue struct {
+	Self   string `json:"self"`
+	ID     string `json:"id"`
+	Key    string `json:"key"`
+	Fields Field  `json:"fields"`
+}
+
+// Field ...
+type Field struct {
+	IssueType IssueType     `json:"issuetype"`
+	Parent    SimpleIssue   `json:"parent"`
+	Priority  Priority      `json:"priority"`
+	Assignee  Person        `json:"assignee"`
+	Status    Status        `json:"status"`
+	Summary   string        `json:"summary"`
+	SubTasks  []SimpleIssue `json:"subtasks"`
+	Reporter  Person        `json:"reporter"`
+	Point     int           `json:"customfield_10002"`
+}
+
+// Priority ...
+type Priority struct {
+	IconURL string `json:"iconUrl"`
+	Name    string `json:"name"`
+}
+
+// Status ...
+type Status struct {
+	IconURL string `json:"iconUrl"`
+	Name    string `json:"name"`
+}
+
+// Person ...
+type Person struct {
+	Name         string     `json:"name"`
+	Key          string     `json:"key"`
+	AvatarUrls   AvatarUrls `json:"avatarUrls"`
+	EmailAddress string     `json:"emailAddress"`
+	DisplayName  string     `json:"displayName"`
+}
+
+// AvatarUrls ...
+type AvatarUrls struct {
+	ExtraSmall string `json:"16x16"`
+	Small      string `json:"24x24"`
+	Medium     string `json:"32x32"`
+	Large      string `json:"48x48"`
+}
+
+// IssueType ...
+type IssueType struct {
+	IconURL string `json:"iconUrl"`
+	Name    string `json:"name"`
+	SubTask bool   `json:"subtask"`
+}
---
+++ session.go
@@ -0,0 +1,27 @@
+package main
+
+// Session ...
+type Session struct {
+	Name  string `json:"name"`
+	Value string `json:"value"`
+}
+
+// LoginInfo ...
+type LoginInfo struct {
+	FailedLoginCount    int    `json:"failedLoginCount"`
+	LoginCount          int    `json:"loginCount"`
+	LastFailedLoginTime string `json:"lastFailedLoginTime"`
+	PreviousLoginTime   string `json:"previousLoginTime"`
+}
+
+// Authentication ...
+type Authentication struct {
+	Session   Session   `json:"session"`
+	LoginInfo LoginInfo `json:"loginInfo"`
+}
+
+// User ...
+type User struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
